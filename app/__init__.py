6823e89792e7c5cd4bb256a088b26c4b018f0eb4
raghuroh <vv.raghurohith@gmail.com>
2018-09-19 12:40:01 -0700

    Logging and profile page done

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from logging import FileHandler, WARNING
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+file_handler = FileHandler('ERRORLOG.TXT')
+file_handler.setLevel(WARNING)
+app.logger.addHandler(file_handler)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+bootstrap = Bootstrap(app)
+
+import routes, models
+
